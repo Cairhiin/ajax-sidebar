21d69ebbff66c9fb5abc4c0dc00b28ca7cf3626a
Frank van de Voorde <cairhiin@gmail.com>
2019-09-18 12:50:33 +0300

    Update ajax-sidebar.php
    Refactoring some code

--- ajax-sidebar.php
+++ ajax-sidebar.php
@@ -1,87 +1,91 @@
 <?php
-/**
- * Plugin Name: ajax-sidebar
- * Plugin URI:
- * Description: Load more Articles with Ajax.
- * Version: 1.0.0
- * Author: Frank van de Voorde
- * Author URI:
- * License: GPL2
- */
+  /**
+   * Plugin Name: ajax-sidebar
+   * Plugin URI:
+   * Description: Load more Articles with Ajax.
+   * Version: 1.0.0
+   * Author: Frank van de Voorde
+   * Author URI:
+   * License: GPL2
+   */
 
- add_action('wp_ajax_get_sidebar_posts', 'get_sidebar_posts');
- add_action('wp_ajax_nopriv_get_sidebar_posts', 'get_sidebar_posts');
- function get_sidebar_posts() {
-    $paged = $_REQUEST['page'];
-    $type = $_REQUEST['type'];
-    $data = '';
+   add_action('wp_ajax_get_sidebar_posts', 'get_sidebar_posts');
+   add_action('wp_ajax_nopriv_get_sidebar_posts', 'get_sidebar_posts');
+   function get_sidebar_posts() {
+      $paged = $_REQUEST['page'];
+      $type = $_REQUEST['type'];
+      $data = '';
+      $meta = '';
 
-    if ($type == 1) :
-      $the_query = new WP_Query(
-   		 array(
-   			 'posts_per_page' => 5,
-   			 'cat' => 5,
-         'paged' => $paged,
-   			 'orderby' => 'date',
-   			 'order'   => 'DESC'
-   	 	 )
-   	  );
-    else:
-      $the_query = new WP_Query(
-				array(
-					'posts_per_page' => 5,
-					'cat' => 5,
-          'paged' => $paged,
-					'meta_key' =>
-					'wpb_post_views_count',
-					'orderby' => 'meta_value_num',
-					'order' => 'DESC'
-				)
-			);
-    endif;
-
- 	if( $the_query->have_posts() ) :
-     while( $the_query->have_posts() ): $the_query->the_post();
-
-      $image = get_the_post_thumbnail_url();
-      if ($image):
-        $image = '<img src="' . $image .'" alt="' . get_the_title() . '" />';
+      if ($type == 1):
+        // Load the newest articles
+        $type = 'date';
+        $html = '<div class="sidebar__content newest-stories">';
+        $the_query = new WP_Query(
+     		 array(
+     			 'posts_per_page' => 5,
+     			 'cat' => 5,
+           'paged' => $paged,
+     			 'orderby' => 'date',
+     			 'order'   => 'DESC'
+     	 	 )
+     	  );
       else:
-        $image = '<img src="' . get_template_directory_uri() . '/images/default.jpg" alt="' . get_the_title() . '" />';
+        // Load the most popular articles
+        $html = '<div class="sidebar__content">';
+        $the_query = new WP_Query(
+  				array(
+  					'posts_per_page' => 5,
+  					'cat' => 5,
+            'paged' => $paged,
+  					'meta_key' =>
+  					'wpb_post_views_count',
+  					'orderby' => 'meta_value_num',
+  					'order' => 'DESC'
+  				)
+  			);
       endif;
 
-      $data = $data .
-        '<div class="sidebar__content">
-  				<a href="">
-  					<div class="sidebar__content__text">
-              '
-              . get_the_title() .
-              '
-  					</div>
-  					<div class="sidebar__content__image">
-              '
-              . $image .
-              '
-  					</div>
-  				</a>
-  			</div>';
+   	if($the_query->have_posts()):
+       while($the_query->have_posts()): $the_query->the_post();
 
-     endwhile;
- 	endif;
+        $image = get_the_post_thumbnail_url();
+        if ($image):
+          $image = '<img src="' . $image .'" alt="' . get_the_title() . '" />';
+        else:
+          $image = '<img src="' . get_template_directory_uri() . '/images/default.jpg" alt="' . get_the_title() . '" />';
+        endif;
 
-  $result['data'] = $data;
-  $result = json_encode($result);
- 	echo $result;
+        $data = $data . $html .
+          ' <a href="">
+    					<div class="sidebar__content__text">
+                '
+                . get_the_title() .
+                '
+    					</div>
+    					<div class="sidebar__content__image">
+                '
+                . $image .
+                '
+    					</div>
+    				</a>
+    			</div>';
 
- 	die();
- }
+       endwhile;
+   	endif;
 
- add_action( 'init', 'script_enqueuer' );
- function script_enqueuer() {
-    wp_register_script( 'addSidebarContent', plugin_dir_url(__FILE__).'ajax-sidebar.js', array('jquery', 'fontawesome') );
-    wp_localize_script( 'addSidebarContent', 'addSidebarContent', array( 'ajaxurl' => admin_url( 'admin-ajax.php' )));
-    wp_enqueue_script( 'jquery' );
-    wp_enqueue_script( 'addSidebarContent' );
- }
+    $result['data'] = $data;
+    $result = json_encode($result);
+   	echo $result;
 
+   	die();
+   }
+
+   add_action( 'init', 'script_enqueuer' );
+   function script_enqueuer() {
+      wp_register_script( 'addSidebarContent', plugin_dir_url(__FILE__).'ajax-sidebar.js', array('jquery', 'fontawesome') );
+      wp_localize_script( 'addSidebarContent', 'addSidebarContent', array( 'ajaxurl' => admin_url( 'admin-ajax.php' )));
+      wp_enqueue_script( 'jquery' );
+      wp_enqueue_script( 'addSidebarContent' );
+   }
  ?>
